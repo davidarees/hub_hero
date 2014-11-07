0f185ca6df071331f309e7e54df14e4f3e6f3c3e
David Rees <davidarees@outlook.com>
2014-11-07 16:07:21 +0000

    created for testing pushing to both repositories in github and heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HubHero::Application.config.secret_token = '29ce9fe5e8aa8f6dcfe86fe2251d490f1d412e33a8b1d67870cd6e32ad7cfc7cf38dbaaf5ade9260560cb2508b0730d8e4912d51c4154b37525c66715a50ae73'
